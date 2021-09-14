1dc19bd5a477719fe213f5c01cf69fcba78bcdee
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-13 22:22:01 -0300

    Update cadrasto.jl

--- cadrasto.jl
+++ cadrasto.jl
@@ -10,7 +10,7 @@ using MySQL
 using JSON
 using DataFrames
 using JSONTables
-include("bd_cadrato.jl")
+#include("bd_cadrato.jl")
 #include("bd_endereco.jl")
 #include("endereco.jl")
 #include("conta.jl")
@@ -37,12 +37,12 @@ route("/criarUser", method = POST) do
     if (verifcar_num(cpf) == false ) || (length(cpf) != 11)
       return "CPF invalido"
     
-    elseif bd_cadrato.verificar_existencia("cpf",cpf) == true
+    #elseif bd_cadrato.verificar_existencia("cpf",cpf) == true
     
-      return "CPF já cadrastado"
+     # return "CPF já cadrastado"
 
-    elseif bd_cadrato.verificar_existencia("email",email) == true
-      return "email já cadrastado"
+   # elseif bd_cadrato.verificar_existencia("email",email) == true
+     # return "email já cadrastado"
     
     #elseif confir_Email.enviar_email(email) != codigo 
       # return "codigo de confirmação incorreto"
@@ -50,9 +50,9 @@ route("/criarUser", method = POST) do
     elseif (verifcar_num(telefone) == false ) || (length(telefone) != 11)
         return "telefone invalido"
 
-    elseif bd_cadrato.verificar_existencia("telefone",telefone) == true
+   # elseif bd_cadrato.verificar_existencia("telefone",telefone) == true
     
-      return "telefone já cadrastado"
+     # return "telefone já cadrastado"
 
     elseif length(senha) < 8 
       return "Senha deve conter mais de 8 caracteres"
@@ -60,12 +60,12 @@ route("/criarUser", method = POST) do
     elseif (verifcar_num(senha_cartao) == false ) || (length(senha_cartao) != 6)
         return "senha deve conter 6 numero "
 
-    else
-      bd_cadrato.insert(cpf , nome , senha, email , telefone ,senha_cartao)
+   # else
+    #  bd_cadrato.insert(cpf , nome , senha, email , telefone ,senha_cartao)
       # dados = test_bd.consultar("cpf" , cpf)
       # bd_endereco.inseir_id(dados.id_cliente)
       # conta.inseir_id(dados.id_cliente)
-    end
+  #  end
     return "POST OK"
 
 end
