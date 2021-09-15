dbf993b0699f785c2ed15d2a0f1496776268f645
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-15 19:20:55 -0300

    Delete transacoes.jl

--- transacoes.jl
+++
@@ -1,8 +0,0 @@
-using SQLite
-using DataFrames
-
-function  atualiza_endere(id_cliente ,coluna , linha)
-
-
-    SQLite.execute(db,"UPDATE endereco SET $coluna = '$linha' WHERE id_cliente = $id_cliente")
-end    
