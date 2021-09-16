2eb6c05ee419882543636ce0e72fe8965163f5ec
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-16 09:29:18 -0300

    Update bd_cadrato.jl

--- bd_cadrato.jl
+++ bd_cadrato.jl
@@ -4,7 +4,7 @@ using MySQL
 using DBInterface
 using DataFrames
 
-db = DBInterface.connect(MySQL.Connection, "us-cdbr-east-04.cleardb.com", "be33b42da89cde", "767dbcfc" , port=3306 , reconnect = true ,connect_timeout = 3600 )
+db = DBInterface.connect(MySQL.Connection, "us-cdbr-east-04.cleardb.com", "be33b42da89cde", "767dbcfc" , port=3306 , reconnect = true ,connect_timeout = 360 )
 
 DBInterface.execute(db, "use heroku_3761ec7676be692")
 DBInterface.execute(db, """CREATE TABLE IF NOT EXISTS conta
