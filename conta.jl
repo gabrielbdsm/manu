d42c7f607dce1c8283d1b692c6c174b081c650cc
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-15 21:48:00 -0300

    Add files via upload

--- conta.jl
+++ conta.jl
@@ -1,27 +1,41 @@
 module conta
-using SQLite
+using MySQL
+using DBInterface
 using DataFrames
 
 
-#conectar/criar banco de dados
-db = SQLite.DB("client.db")
+# #conectar/criar banco de dados
+# db = SQLite.DB("client.db")
 
-# criar TABLE
-SQLite.execute(db,"CREATE TABLE IF NOT EXISTS conta(
-    id_cliente INTEGER REFERENCES dados (id_cliente) ON DELETE CASCADE
-                                                     ON UPDATE CASCADE
-                       UNIQUE,
-    n_conta    INTEGER PRIMARY KEY AUTOINCREMENT
-                       UNIQUE,
-    saldo      REAL    DEFAULT (0),
-    agencia    TEXT
-)")
+# # criar TABLE
+# SQLite.execute(db,"CREATE TABLE IF NOT EXISTS conta(
+#     id_cliente INTEGER REFERENCES dados (id_cliente) ON DELETE CASCADE
+#                                                      ON UPDATE CASCADE
+#                        UNIQUE,
+#     n_conta    INTEGER PRIMARY KEY AUTOINCREMENT
+#                        UNIQUE,
+#     saldo      REAL    DEFAULT (0),
+#     agencia    TEXT
+# )")
+db = DBInterface.connect(MySQL.Connection, "us-cdbr-east-04.cleardb.com", "be33b42da89cde", "767dbcfc" , port=3306 , reconnect = true ,connect_timeout = 3600 )
 
+DBInterface.execute(db, "use heroku_3761ec7676be692")
+DBInterface.execute(db, """CREATE TABLE IF NOT EXISTS conta
+(
+    id_cliente INT ,
+    n_conta     INT NOT NULL AUTO_INCREMENT,
+    agencia     varchar(255) DEFAULT '0001',
+    saldo FLOAT DEFAULT 0,
+    PRIMARY KEY (n_conta),
+    FOREIGN KEY (id_cliente) REFERENCES dados(id_cliente) ON DELETE CASCADE
+    
+                )AUTO_INCREMENT = 100000000;""")
+                
 
 
 
 function inseir_id(id_cliente)
-    SQLite.execute(db,"INSERT INTO conta(id_cliente , agencia) VALUES ($id_cliente , '0001')")
+    DBInterface.execute(db,"INSERT INTO conta(id_cliente ) VALUES ($id_cliente)")
 end
 
 
@@ -29,7 +43,7 @@ end
 function verificar_existencia(coluna , linha )
     try
         
-            select =SQLite.DBInterface.execute(db, "SELECT $coluna FROM conta WHERE $coluna = '$linha'")
+            select =DBInterface.execute(db, "SELECT $coluna FROM conta WHERE $coluna = '$linha'")
             select = DataFrames.DataFrame(select)
             select = Tuple(select[1,:])
         catch
@@ -43,7 +57,7 @@ end
 
 function  consultar(coluna , linha)
     if  verificar_existencia(coluna , linha) == true
-    select =SQLite.DBInterface.execute(db, "SELECT * FROM conta WHERE $coluna = '$linha'")
+    select = DBInterface.execute(db, "SELECT * FROM conta WHERE $coluna = '$linha'")
     select = DataFrames.DataFrame(select)
     select= NamedTuple(select[1,:])
     
@@ -51,8 +65,9 @@ function  consultar(coluna , linha)
    else
         return -1
      end
+
+end
+
 end
 
 
-
-end
--- login.jl
+++ login.jl
@@ -5,7 +5,7 @@ using JSON
 using DataFrames
 using JSONTables
 include("bd_cadrato.jl")
-#include("conta.jl")
+include("conta.jl")
 
 
 #ic-banking-2021 
@@ -16,24 +16,25 @@ route("/login" , method = POST) do
     cpf = postpayload(:cpf)
     senha = postpayload(:senha)
     usuario = bd_cadrato.consultar("cpf",cpf)
-    
+  
     if usuario == -1
-        return "CPF NÃO cadrastada  "
+        return "CPF NÃO cadrastada"
     elseif senha != usuario.senha
         return "senha incorreta"
-    # else
+    else
 
-    #     dados = conta.consultar("id_cliente",11)
-    #     dados = JSON.json(dados)
-    #     dados = JSON.Parser.parse(dados)
-    #     dados["cpf"] = cpf
+        dados = conta.consultar("id_cliente",usuario.id_cliente)
+        dados = JSON.json(dados)
+        dados = JSON.Parser.parse(dados)
+        dados["cpf"] = cpf
         
         
-    #     return  JSON.json(dados)
+        return  JSON.json(dados)
     end
         
 end
   
 
+up(8002, async = false)
 
 end
