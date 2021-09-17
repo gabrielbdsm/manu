7494a8a6889ab369a99736c751214cbad20097b2
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-17 10:58:28 -0300

    Update cadrasto.jl

--- cadrasto.jl
+++ cadrasto.jl
@@ -6,8 +6,8 @@ using JSONTables
 
 include("bd_cadrato.jl")
 include("login.jl")
-#include("bd_endereco.jl")
-#include("endereco.jl")
+include("bd_endereco.jl")
+include("endereco.jl")
 include("conta.jl")
 #include("confir_Email.jl")
 
